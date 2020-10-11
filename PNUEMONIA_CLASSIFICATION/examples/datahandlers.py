1199814104c634c39c342c1cdd10155c434e0e3e
SAMISHSALGAONKAR <72574645+SAMISHSALGAONKAR@users.noreply.github.com>
2020-10-11 18:13:42 +0530

    Add files via upload

---
+++ PNUEMONIA_CLASSIFICATION/examples/datahandlers.py
@@ -0,0 +1,20 @@
+import os
+
+
+def get_datahandler_config(dh_name, folder_data, party_id, is_agg):
+    if dh_name == 'xray':
+        data = {
+            'name': 'XrayKerasDataHandler',
+            'path': 'ibmfl.util.data_handlers.xray_keras_data_handler',
+            'info': {
+                'npz_file': os.path.join(folder_data, 'data_party' + str(party_id) + '.npz')
+            }
+        }
+        if is_agg:
+            data['info'] = {
+                'npz_file': os.path.join("examples", "datasets", "xray.npz")
+            }
+      
+
+
+    return data
